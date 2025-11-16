c5558ac454a974896c53c292ac3d1835cd8eded8
Banana-Master <tjdgh0313@gmail.com>
2025-11-16 18:16:21 +0900

    Next-Auth 로그인 로직 추가

---
+++ frontend/src/lib/password-utils.ts
@@ -0,0 +1,14 @@
+import bcrypt from 'bcryptjs';
+
+export function saltAndHashPassword(password: string): string {
+  const saltRounds = 10;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(password, salt);
+
+  return hash;
+}
+
+/** DB에 있는 비밀번호 vs 입력한 비밀번호 일치 여부 확인 */
+export function comparePassword(password: string, hashedPassword: string): boolean {
+  return bcrypt.compareSync(password, hashedPassword);
+}
